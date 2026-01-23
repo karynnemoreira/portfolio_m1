0c39cb6c8925b23bcf5b2fee21132adc639860cf
karynnemoreira <karynnemoreira@yahoo.com.br>
2026-01-23 10:32:13 -0300

    Revisão rotas, introdução scss em react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Projetos from "./components/Projetos/Projetos.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
+import "./global.scss"
 
 function App() {
   return (
     <>
       <NavBar />
-      {/* <Projetos /> */}
+       {/* <Projetos />  */}
     </>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,37 +1,39 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Projetos from "../Projetos/Projetos"
-import Inicio from "../Inicio/Inicio"
-import Contatos from "../Contados/Contatos"
 
 //BrowserRouter: Container que vai envolver tudo relacionado as nossas rotas. E vai habilitar essa funcionalidade.
+//Routes: É uma caixa que vai envolver todas as rotas
+//Route: <Route /> Define uma rota individual. Nele que vamos chamar os components.
+//Link: É usado para criar links entre as páginas. Ela não sai do site.
 
-//Routes: Uma caixa que vai envolver todas as rotas.  
+import Inicio from "../Inicio/Inicio";
+import Projetos from "../Projetos/Projetos";
+import Contatos from "../Contatos/Contatos";
 
-//Route <Route /> Define uma rota individual. Nele que vamos chamar os componentes. Ex: <Route path="/projetos" element={<Projetos />} />
+import styles from "./NavBar.module.scss"
 
-//Link é usado para criar links entre as páginas, substituindo a tag <a>.
 
 function NavBar() {
   return (
     <BrowserRouter>
 
-     <nav>
-      <img src="https://play-lh.googleusercontent.com/E6RS-I1T58nvom_QfR7gondwJ1OWqfv2c8lz09FviTc9XWjrNMCl4XiAZD0-6qb0Tw=w256" alt="Logo" />
-      <ul>
-        <li> <Link to="/"> Início   </Link> </li>
-        <li> <Link to="/projetos"> Projetos </Link> </li>
-        <li> <Link to="/contatos"> Contatos </Link> </li>
-      </ul>
-    </nav>
+         <nav className={styles.navBarHeader}>
+        <img
+          src="https://play-lh.googleusercontent.com/E6RS-I1T58nvom_QfR7gondwJ1OWqfv2c8lz09FviTc9XWjrNMCl4XiAZD0-6qb0Tw=w256"
+          alt="Logo"
+        />
+        <ul>
+          <li> <Link to="/" > Início </Link>  </li>
+          <li> <Link to="/projetos" > Projetos </Link>  </li>
+          <li> <Link to="/contatos">  Contatos</Link> </li>
+        </ul>
+      </nav>
 
-<Routes> 
-    <Route path="/" element={<Inicio />} /> 
-    <Route path="/projetos" element={<Projetos />} />
-    <Route path="/contatos" element={<Contatos/>} />
-</Routes>
-
+      <Routes>
+        <Route path="/" element={ <Inicio /> } />
+        <Route path="/projetos" element={ <Projetos />} />
+        <Route path="/contatos" element={ <Contatos /> } />
+      </Routes>
     </BrowserRouter>
-   
   );
 }
 
